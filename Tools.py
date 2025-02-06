41e9d8bc3c4c77fb5a011e139170e94c8bb69be5
tomabascal <130658470+tomabascal@users.noreply.github.com>
2025-02-06 23:45:24 +0000

    h

--- Tools.py
+++ Tools.py
@@ -97,7 +97,7 @@ def process_files(ppt_file, excel_file, search_option, start_row, end_row, store
 
     # Aplicar filtros según la opción seleccionada
     if search_option == 'rows':
-        df_selected = df1.iloc[start_row-2:end_row-2]
+        df_selected = df1.iloc[start_row-2:end_row]
     elif search_option == 'store_id':
         store_id_list = [store_id.strip() for store_id in store_ids.split(',')]
         df_selected = df1[df1.iloc[:, 0].astype(str).isin(store_id_list)]
