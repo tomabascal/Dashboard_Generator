e9a6ed893469c9c3d9227d60f4e93b286f6b567d
tomabascal <130658470+tomabascal@users.noreply.github.com>
2025-02-11 11:24:22 +0000

    cambio en rows de previous

--- PreviousCode.py
+++ PreviousCode.py
@@ -85,7 +85,7 @@ def process_files(ppt_file, excel_file, search_option, start_row, end_row, store
         return
 
     if search_option == 'rows':
-        df_selected = df1.iloc[start_row:end_row + 1]
+        df_selected = df1.iloc[start_row-2:end_row-1]
     elif search_option == 'store_id':
         store_id_list = [store_id.strip() for store_id in store_ids.split(',')]
         df_selected = df1[df1.iloc[:, 0].astype(str).isin(store_id_list)]
